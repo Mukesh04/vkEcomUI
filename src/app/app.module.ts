edebbdd775cc3a0c479a92256043aac3b7f116dd
Lazeapostolski <48823607+Lazeapostolski@users.noreply.github.com>
2019-07-01 14:41:08 +0200

    details

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { DemoComponent } from './components/demo/demo.component';
 import { BlogColumnComponent } from './components/blog/blog-column/blog-column.component';
+import { BlogDetailsComponent } from './components/blog/blog-details/blog-details.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     DemoComponent,
-    BlogColumnComponent
+    BlogColumnComponent,
+    BlogDetailsComponent
   ],
   imports: [
     BrowserModule
