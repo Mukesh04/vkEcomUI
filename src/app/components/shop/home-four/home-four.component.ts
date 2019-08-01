efa459752b051a175270a44b9255c65f107d6472
lamarena <lazeapostolski@gmail.com>
2019-08-01 08:27:38 +0200

    home 4

--- src/app/components/shop/home-four/home-four.component.ts
+++ src/app/components/shop/home-four/home-four.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Product } from 'src/app/modals/product.model';
 import { ProductService } from '../../shared/services/product.service';
+import { CartItem } from 'src/app/modals/cart-item';
+import { CartService } from '../../shared/services/cart.service';
+
 
 
 @Component({
@@ -10,7 +13,24 @@ import { ProductService } from '../../shared/services/product.service';
 })
 export class HomeFourComponent implements OnInit {
 
+  public currencies = ['USD', 'EUR'];
+  public currency:any;
+  public flags = [
+    { name:'English', image: 'assets/images/flags/gb.svg' },
+    { name:'German', image: 'assets/images/flags/de.svg' },
+    { name:'French', image: 'assets/images/flags/fr.svg' },
+    { name:'Russian', image: 'assets/images/flags/ru.svg' },
+    { name:'Turkish', image: 'assets/images/flags/tr.svg' }
+  ]
+  public flag:any;
+
   products: Product[];
+
+  indexProduct: number;
+  shoppingCartItems: CartItem[] = [];
+  wishlistItems  :   Product[] = [];
+
+
   public banners = [];
   public slides = [
     { title: 'Huge sale', subtitle: 'Up to 70%', image: 'assets/images/carousel/banner1.jpg' },
@@ -20,7 +40,10 @@ export class HomeFourComponent implements OnInit {
     { title: 'Massive sale', subtitle: 'Only for today', image: 'assets/images/carousel/banner5.jpg' }
   ];
 
-  constructor(private productService: ProductService) { }
+  constructor(private productService: ProductService,  private cartService: CartService) {
+    this.cartService.getItems().subscribe(shoppingCartItems => this.shoppingCartItems = shoppingCartItems);
+
+  }
 
   ngOnInit() {
     this.productService.getBanners()
@@ -34,7 +57,19 @@ export class HomeFourComponent implements OnInit {
      this.products = product
    }
  )
+ this.currency = this.currencies[0];
+  this.flag = this.flags[0];
+}
 
+
+
+
+
+public changeCurrency(currency){
+  this.currency = currency;
+}
+public changeLang(flag){
+  this.flag = flag;
 }
 
 }
