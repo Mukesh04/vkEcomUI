7cd6be26181a6556a8d0af430f37ab98467525eb
yogeshds10 <yogeshds10@gmail.com>
2019-12-12 14:22:11 +0530

    default to home four as main page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,11 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { DemoComponent } from './components/demo/demo.component';
-import { NgxSpinnerModule } from "ngx-spinner";
+// import { DemoComponent } from './components/demo/demo.component';
+import { NgxSpinnerModule } from 'ngx-spinner';
 import { NgxImgZoomModule } from 'ngx-img-zoom';
 
-
 import { MainComponent } from './components/main/main.component';
-
-
 
 import { AppRoutingModule } from './app-routing.module';
 import { ShopModule } from './components/shop/shop.module';
@@ -21,11 +18,7 @@ import { SharedModule } from './components/shared/shared.module';
 @NgModule({
   declarations: [
     AppComponent,
-    DemoComponent,
     MainComponent
-
-
-
   ],
   imports: [
     NgxSpinnerModule,
